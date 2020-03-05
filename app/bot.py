00cd356d331fe9ad950aabfe5d950a0202cc8de2
Tran Viet Phuoc <phuoc.finn@gmail.com>
2020-03-06 01:34:08 +0700

    complete bot

--- app/bot.py
+++ app/bot.py
@@ -5,22 +5,30 @@ from app.utils import (generate_list_items, save_data,
                        load_data)
 from app.models import session
 from datetime import datetime
+import logging
 
 
 URL = 'https://vnexpress.net'
 
 MY_TOKEN = '984695585:AAHE0knIkpzxo6SkeK55bgemJLz4L_elTHk'
 
+# use logging module to know when things don't work as expected
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
 
 @run_async
-def news(update: Update, context: CallbackContext):
+def news(update, context):
     info = load_data(datetime.utcnow())
     # print(info)
     chat_id = update.message.chat_id
-
+    # if content is None then pass
     for i in info:
-        message = i.content + ' - ' + i.link
-        context.bot.send_message(chat_id=chat_id, message=message)
+        if i.content is None:
+            continue
+        else:
+            message = (i.content + ' - ' + i.link)
+            context.bot.send_message(chat_id=chat_id, text=message)
 
 
 def main():
@@ -29,4 +37,3 @@ def main():
     dp.add_handler(CommandHandler('news', news))
     updater.start_polling()
     updater.idle()
-
