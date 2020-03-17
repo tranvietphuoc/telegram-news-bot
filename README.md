1b4498a4df366dfc2a6db92e72bc91de495b316b
Tran Viet Phuoc <phuoc.finn@gmail.com>
2020-03-18 01:54:54 +0700

    pytest

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 * [python](https://www.python.org "python") version 3
 * [pipenv](https://pipenv.pypa.io/en/latest/ "pipenv") - you can install it by following the documentation
 ## Dependencies
-* `pyhon 3.8.2`
+* `python 3.8.2`
 * `beautifulsoup4`
 * `requests`
 * `python-telegram-bot`
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,26 @@
 import pytest
+from app import session_scope
+from app.utils import get_soup
+from app.models import engine, Info
 
 
 @pytest.fixture(scope='module')
-def 
+def test_request():
+    assert get_soup('https://vnexpress.net').status_code == 200
+
+
+def init_db():
+    with session_scope() as session:
+        info = Info(content='test', link='link.test')
+        session.add(info)
+        session.commit()
+
+
+@pytest.fixture(scope='module')
+def test_db():
+    init_db()
+    info_test = Info(content='test', link='link.test')
+    
+    with session_scope() as session:
+        assert info_test == session.query(Info).filter(
+            Info.content == 'test').all()
